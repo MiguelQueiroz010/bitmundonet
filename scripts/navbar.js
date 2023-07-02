bf8792062154e44b5c090a854f0c1bc946859af9
Miguel Queiroz <miguelqueiroz010@gmail.com>
2023-07-02 15:15:41 -0300

    fix-scrolling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,12 +1,18 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+
+var maxH = window.innerHeight * 0.6;
+var minH = window.innerHeight * 0.4;
+
+  if (window.scrollY >= maxH) {
     document.getElementById("navbar").style.top = "0";
     document.getElementById("navbar").style.position = "fixed";
     document.getElementById("banner").style.display="none";
 
-  } else {
+  } 
+  if(window.scrollY <= minH) {
     document.getElementById("navbar").style.position = "relative";
     document.getElementById("banner").style.display="inline-block";
 

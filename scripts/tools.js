a50292eed9722fc94b62b619f8c6aaea99fa897d
Miguel <miguelqueiroz010@gmail.com>
2025-02-02 20:05:36 -0300

    tools_added_populated

--- scripts/tools.js
+++ scripts/tools.js
@@ -25,6 +25,10 @@ function getTools(xmlDoc) {
 
     for (var i = 0; i < tools.length; i++) {
         var tool = tools[i];
+        var toolExtra = "";
+        if(tool.getElementsByTagName("extra").length > 0){
+            var toolExtra = tool.getElementsByTagName("extra")[0].childNodes[0].nodeValue;
+        }   
         var toolName = tool.getAttribute("nome");
         var toolLink = tool.getElementsByTagName("url")[0].childNodes[0].nodeValue;
         var toolDescription = tool.getElementsByTagName("description")[0].childNodes[0].nodeValue;
@@ -32,6 +36,7 @@ function getTools(xmlDoc) {
         var toolVersion = tool.getElementsByTagName("version")[0].childNodes[0].nodeValue;
         var toolType = tool.getElementsByTagName("type")[0].childNodes[0].nodeValue;
         var toolTarget = tool.getElementsByTagName("target")[0].childNodes[0].nodeValue;
+        var toolCredit = tool.getElementsByTagName("credit")[0].childNodes[0].nodeValue;
 
         toolArray.push({
             name: toolName,
@@ -40,7 +45,9 @@ function getTools(xmlDoc) {
             image: toolImage,
             version: toolVersion,
             type: toolType,
-            target: toolTarget
+            target: toolTarget,
+            credits: toolCredit,
+            extraLink: toolExtra
         });
     }
 
@@ -51,7 +58,9 @@ function getTools(xmlDoc) {
     for (var i = 0; i < toolArray.length; i++) {
         var toolItem = document.createElement("div");
         toolItem.className = "toolItem";
-        toolItem.innerHTML = `<a href="#info" onclick="folder(this)">
+        toolItem.innerHTML = `<a style="cursor: pointer;" 
+        
+        onclick="folder(this)">
             <picture id="hide_folder" >
             <source srcset="../../elements/down_arrow.png" media="(prefers-color-scheme: dark)" />
             <img id="hide_folder" src="../../elements/light_down_arrow.png" />
@@ -60,11 +69,13 @@ function getTools(xmlDoc) {
             <h2>${toolArray[i].name}</h2>
             </a>
              <div id="info" style="display: none;">
-            <p>${toolArray[i].description}
+            <p>${toolArray[i].description}<a target="_blank" href="${toolArray[i].extraLink}">${toolArray[i].extraLink}</a>
             <br><br>
             Tipo de Ferramenta: ${toolArray[i].type}
             <br><br>
             Alvo:<span style="color:red"> ${toolArray[i].target}</span>
+            <br><br>
+            Créditos:<span style="color:green"> ${toolArray[i].credits}</span>
             </p>
             <div id="link">
             <a href="${toolArray[i].link}" target="_blank">
@@ -91,7 +102,6 @@ function getTools(xmlDoc) {
 }
 function folder(el) {
     var info = el.nextElementSibling;
-    console.log(el);
     if (info.style.display === "none") {
         info.style.display = "block";
         el.children[0].children[1].style.transform = "scaleY(-1)";

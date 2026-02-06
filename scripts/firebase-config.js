5621381d86249b654e6879ffbf284ad7a4126d8f
MiguelQueiroz010 <MiguelQueiroz010@users.noreply.github.com>
2026-02-06 00:03:59 +0000

    deploy: dcfe914a539e5702a4bf603e8cb7a9e21483ecd6

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+/**
+ * Firebase Configuration Template for BitMundo
+ * 
+ * PLACEHOLDERS will be replaced during the GitHub Actions build process.
+ */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWN9KX9pUKvX1VsMbzzBxgOhugaLtOk3k",
+    authDomain: "bitraiden-site.firebaseapp.com",
+    projectId: "bitraiden-site",
+    storageBucket: "bitraiden-site.firebasestorage.app",
+    messagingSenderId: "231370595400",
+    appId: "1:231370595400:web:14fd476849a42d3f0a4c7d",
+    measurementId: ""
+};
+
+export default firebaseConfig;

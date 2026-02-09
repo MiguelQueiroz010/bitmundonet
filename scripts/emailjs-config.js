dd626912a3b237c7123c1c468c219671392a619c
MiguelQueiroz010 <MiguelQueiroz010@users.noreply.github.com>
2026-02-09 15:43:49 +0000

    deploy: d08568fc27169bdc77c2b73c0278dd935b400c32

---
+++ scripts/emailjs-config.js
@@ -0,0 +1,13 @@
+/**
+ * EmailJS Configuration Template for BitMundo
+ * 
+ * PLACEHOLDERS will be replaced during the GitHub Actions build process.
+ */
+
+const emailjsConfig = {
+    SERVICE_ID: "gmail_notifycomments",
+    TEMPLATE_ID: "template_1fk6fir",
+    PUBLIC_KEY: "cqKUD2bTo2tZth8RZ"
+};
+
+export default emailjsConfig;
